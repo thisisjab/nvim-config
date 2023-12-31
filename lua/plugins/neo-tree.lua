58d7216730e61af0624fe0cb88901c499d4be0b2
thisisjab <heythisisjab@gmail.com>
2024-01-01 01:24:34 +0330

    feat: configure `neo-tree`

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,5 +6,52 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
       "3rd/image.nvim",
-    }
+    },
+    config = function()
+        -- Mappings
+        vim.api.nvim_set_keymap('n', '<leader>ft', ':Neotree<CR>', { noremap = true, silent = true })
+
+        -- Configuration
+        require("neo-tree").setup({
+            filesystem = {
+          filtered_items = {
+            visible = false, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = false,
+            hide_gitignored = false,
+            hide_hidden = false, 
+            hide_by_name = {
+              --"node_modules"
+            },
+            hide_by_pattern = { -- uses glob style patterns
+              --"*.meta",
+              --"*/src/*/tsconfig.json",
+            },
+            always_show = { -- remains visible even if other settings would normally hide it
+              --".gitignored",
+            },
+            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+              --".DS_Store",
+              --"thumbs.db"
+            },
+            never_show_by_pattern = { -- uses glob style patterns
+              --".null-ls_*",
+            },
+          },
+            },
+
+        window = {
+            mappings = {
+['<tab>'] = function (state)
+  local node = state.tree:get_node()
+  if require("neo-tree.utils").is_expandable(node) then
+    state.commands["toggle_node"](state)
+  else
+    state.commands['open'](state)
+    vim.cmd('Neotree reveal')                  
+  end
+end,
+                }
+        }
+        })
+    end
 }
