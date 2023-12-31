c90b97590b6c8fc33c60a467a2fa0c4b6224875f
thisisjab <heythisisjab@gmail.com>
2024-01-01 01:36:44 +0330

    feat: configure `telescope`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,14 @@
 return {
     -- TODO: setup mappings
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            -- Mappings
+            local builtin = require('telescope.builtin')
+                vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+                vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+                vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
+                vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+                vim.keymap.set('n', '<leader>pb', builtin.git_branches, {})
+        end
     }
