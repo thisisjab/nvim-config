babbe57e8c9b6cb4eb41d01d7d0f2f3836426467
thisisjab <heythisisjab@gmail.com>
2024-01-01 01:57:40 +0330

    feat: add `telescope-ui-select` for `telescope`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,29 @@
 return {
-    -- TODO: setup mappings
-    'nvim-telescope/telescope.nvim', tag = '0.1.5',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            -- Mappings
-            local builtin = require('telescope.builtin')
-                vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-                vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-                vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
-                vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
-                vim.keymap.set('n', '<leader>pb', builtin.git_branches, {})
-        end
+        {
+        'nvim-telescope/telescope.nvim', tag = '0.1.5',
+          dependencies = { 'nvim-lua/plenary.nvim' },
+            config = function()
+                -- Mappings
+                local builtin = require('telescope.builtin')
+                    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+                    vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+                    vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
+                    vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
+                    vim.keymap.set('n', '<leader>pb', builtin.git_branches, {})
+            end
+        },
+        {
+            'nvim-telescope/telescope-ui-select.nvim',
+            config = function()
+                require("telescope").setup {
+                  extensions = {
+                    ["ui-select"] = {
+                      require("telescope.themes").get_dropdown {
+                      }
+                    }
+                  }
+                }
+                require("telescope").load_extension("ui-select")
+            end
+        }
     }
