713f3840c1fa1e77ade543d8bf110a6ffe2090b3
thisisjab <heythisisjab@gmail.com>
2024-01-01 01:09:23 +0330

    chore: add mappings for lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -33,6 +33,45 @@ return {
             }
 
         require("lspconfig").lua_ls.setup {}
+        
+        -- Mappings
+         vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                -- Buffer local mappings.
+                -- See `:help vim.lsp.*` for documentation on any of the below functions
+                local opts = {
+                    buffer = ev.buf
+                }
+                vim.keymap.set({ 'n', 'i' }, '<C-D>', vim.lsp.buf.declaration, opts)
+                vim.keymap.set({ 'n', 'i' }, '<C-d>', vim.lsp.buf.definition, opts)
+                vim.keymap.set({ 'n', 'i' }, '<C-h>', vim.lsp.buf.hover, opts)
+                -- vim.keymap.set({ 'n', 'i' }, '<C-i>', vim.lsp.buf.implementation, opts)
+                vim.keymap.set({ 'n', 'i' }, '<C-k>', vim.lsp.buf.signature_help, opts)
+                -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                -- vim.keymap.set('n', '<space>wl', function()
+                -- print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                -- end, opts)
+                -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set({ 'n', 'i' }, '<F2>', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', }, '<space>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set({ 'n', 'i' }, '<C-r>', vim.lsp.buf.references, opts)
+
+                -- Disabled formattting with lsp since it not always working properly
+                -- I use neoformat
+                -- 
+                -- vim.keymap.set({ 'n', 'i' }, '<C-p>', function()
+                --     vim.lsp.buf.format {
+                --         async = false
+                --     }
+                -- end, opts)
+            end
+        })
+
         end
     },
 
