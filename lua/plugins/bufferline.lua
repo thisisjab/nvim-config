0222297f4ea8fc52acf8afe62aef8b33d84d1bf9
thisisjab <heythisisjab@gmail.com>
2024-01-01 01:59:27 +0330

    feat: install and configure `bufferline`

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,29 @@
+return { {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    event = 'BufReadPre',
+    config = function()
+        require("bufferline").setup {}
+        vim.api.nvim_set_keymap('n', '<M-n>', ':BufferLineCycleNext<CR>', {
+            noremap = true,
+            silent = true
+        })
+        vim.api.nvim_set_keymap('i', '<M-n>', '<Esc>:BufferLineCycleNext<CR>', {
+            noremap = true,
+            silent = true
+        })
+        vim.api.nvim_set_keymap('n', '<M-p>', ':BufferLineCyclePrev<CR>', {
+            noremap = true,
+            silent = true
+        })
+        vim.api.nvim_set_keymap('i', '<M-p>', '<Esc>:BufferLineCyclePrev<CR>', {
+            noremap = true,
+            silent = true
+        })
+        vim.api.nvim_set_keymap('n', '<M-c>', ':bd<CR>', {
+            noremap = true,
+            silent = true
+        })
+    end
+} }
