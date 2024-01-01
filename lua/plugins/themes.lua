efa822be74f32b8dff7e0041b1f803e0470b0478
thisisjab <heythisisjab@gmail.com>
2024-01-02 00:07:47 +0330

    refactor: move all themes to `themes.lua`

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,10 @@
+return {
+    { "catppuccin/nvim",              name = "catppuccin", priority = 1000 },
+    { "miikanissi/modus-themes.nvim", name = "modus",      priority = 1000 },
+    { "olivercederborg/poimandres.nvim", name = "poimandres",      priority = 1000 },
+    { "morhetz/gruvbox", name = "gruvbox",      priority = 1000 },
+    { "nlknguyen/papercolor-theme", name = "papercolor",      priority = 1000 },
+    { "mofiqul/vscode.nvim", name = "vscode",      priority = 1000 },
+    { "oxfist/night-owl.nvim", name = "nightowl",      priority = 1000 },
+    { "ewilazarus/preto", name = "preto",      priority = 1000 },
+}
