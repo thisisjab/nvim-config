6fa6891f42fee8d3a73d64b136aff723ac9b01e6
thisisjab <heythisisjab@gmail.com>
2024-01-02 00:09:13 +0330

    feat: add mapping for selecting colorschemes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,29 +1,35 @@
 return {
-        {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-          dependencies = { 'nvim-lua/plenary.nvim' },
-            config = function()
-                -- Mappings
-                local builtin = require('telescope.builtin')
-                    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-                    vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-                    vim.keymap.set('n', '<leader>ps', builtin.treesitter, {})
-                    vim.keymap.set('n', '<leader>pd', builtin.diagnostics, {})
-                    vim.keymap.set('n', '<leader>pb', builtin.git_branches, {})
-            end
-        },
-        {
-            'nvim-telescope/telescope-ui-select.nvim',
-            config = function()
-                require("telescope").setup {
-                  extensions = {
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            -- Mappings
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>ps", builtin.treesitter, {})
+            vim.keymap.set("n", "<leader>pd", builtin.diagnostics, {})
+            vim.keymap.set("n", "<leader>pb", builtin.git_branches, {})
+            vim.keymap.set("n", "<leader>nvt", builtin.colorscheme, {})
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                pickers = {
+                    colorscheme = {
+                        enable_preview = true,
+                    },
+                },
+                extensions = {
                     ["ui-select"] = {
-                      require("telescope.themes").get_dropdown {
-                      }
-                    }
-                  }
-                }
-                require("telescope").load_extension("ui-select")
-            end
-        }
-    }
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
+}
