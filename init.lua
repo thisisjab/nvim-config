b3d888378bb992449f463d9274d4a02bc5d58106
thisisjab <heythisisjab@gmail.com>
2024-01-08 17:07:19 +0330

    feat: add custom mappings for moving lines up and down

--- init.lua
+++ init.lua
@@ -17,4 +17,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 
+require("mappings")
 require("configurations")
---
+++ lua/mappings.lua
@@ -0,0 +1,5 @@
+-- This file is for custom mappings
+
+-- Move selected lines up and down using J and K
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
