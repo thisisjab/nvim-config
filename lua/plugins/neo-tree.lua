7b833fce9d6aa14d73ba9400daf56f0b25d25709
thisisjab <heythisisjab@gmail.com>
2024-01-23 14:42:28 +0330

    feat: enable auto-refresh in `neo-tree`

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,56 +2,57 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+        "3rd/image.nvim",
     },
     config = function()
         -- Mappings
-        vim.api.nvim_set_keymap('n', '<leader>fe', ':Neotree<CR>', { noremap = true, silent = true })
+        vim.api.nvim_set_keymap("n", "<leader>fe", ":Neotree<CR>", { noremap = true, silent = true })
 
         -- Configuration
         require("neo-tree").setup({
             filesystem = {
-          filtered_items = {
-            visible = false, -- when true, they will just be displayed differently than normal items
-            hide_dotfiles = false,
-            hide_gitignored = false,
-            hide_hidden = false, 
-            hide_by_name = {
-              --"node_modules"
-            },
-            hide_by_pattern = { -- uses glob style patterns
-              --"*.meta",
-              --"*/src/*/tsconfig.json",
-            },
-            always_show = { -- remains visible even if other settings would normally hide it
-              --".gitignored",
-            },
-            never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
-              --".DS_Store",
-              --"thumbs.db"
-            },
-            never_show_by_pattern = { -- uses glob style patterns
-              --".null-ls_*",
-            },
-          },
+                filtered_items = {
+                    visible = false, -- when true, they will just be displayed differently than normal items
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_hidden = false,
+                    hide_by_name = {
+                        --"node_modules"
+                    },
+                    hide_by_pattern = { -- uses glob style patterns
+                        --"*.meta",
+                        --"*/src/*/tsconfig.json",
+                    },
+                    always_show = { -- remains visible even if other settings would normally hide it
+                        --".gitignored",
+                    },
+                    never_show = { -- remains hidden even if visible is toggled to true, this overrides always_show
+                        --".DS_Store",
+                        --"thumbs.db"
+                    },
+                    never_show_by_pattern = { -- uses glob style patterns
+                        --".null-ls_*",
+                    },
+                },
+                use_libuv_file_watcher = true, -- detect file and directory changes
             },
 
-        window = {
-            mappings = {
-['<tab>'] = function (state)
-  local node = state.tree:get_node()
-  if require("neo-tree.utils").is_expandable(node) then
-    state.commands["toggle_node"](state)
-  else
-    state.commands['open'](state)
-    vim.cmd('Neotree reveal')                  
-  end
-end,
-                }
-        }
+            window = {
+                mappings = {
+                    ["<tab>"] = function(state)
+                        local node = state.tree:get_node()
+                        if require("neo-tree.utils").is_expandable(node) then
+                            state.commands["toggle_node"](state)
+                        else
+                            state.commands["open"](state)
+                            vim.cmd("Neotree reveal")
+                        end
+                    end,
+                },
+            },
         })
-    end
+    end,
 }
