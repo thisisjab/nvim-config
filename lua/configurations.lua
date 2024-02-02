e9b7577b4053494e994ba338447929f0ad2a6c18
thisisjab <heythisisjab@gmail.com>
2024-02-03 01:58:53 +0330

    feat: make background transparent

--- lua/configurations.lua
+++ lua/configurations.lua
@@ -3,9 +3,13 @@
 vim.opt.nu = true
 
 vim.opt.termguicolors = true
-vim.cmd("colorscheme moonfly")
+vim.cmd("colorscheme catppuccin-mocha")
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+-- Make background transparent
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
