16030892e84e9b446649a01c98b79e1008249d41
thisisjab <heythisisjab@gmail.com>
2024-02-28 19:51:26 +0330

    feat: change theme to `base16-gruvebox`

--- lua/configurations.lua
+++ lua/configurations.lua
@@ -1,9 +1,15 @@
 -- Keep all vim/neovim configurations here
 
-vim.opt.nu = true
+-- vim.opt.nu = true
+
+-- Fix base16 themese removing borders from telescope
+require('base16-colorscheme').with_config {
+    telescope = false,
+}
 
 vim.opt.termguicolors = true
-vim.cmd("colorscheme catppuccin-mocha")
+
+vim.cmd("colorscheme base16-gruvbox-dark-hard")
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -11,5 +17,5 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Make background transparent
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
