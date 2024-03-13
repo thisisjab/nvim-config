211dfbc022255487e26c0063290f37e5da123f82
thisisjab <heythisisjab@gmail.com>
2024-03-13 22:43:10 +0330

    fix(autopairs): fix `<CR>` ruining indentation

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -188,5 +188,35 @@ return {
         keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
         -- Resume latest coc list (Disable since it interferes with telescope)
         -- keyset("n", "<space>p", ":<C-u>CocListResume<cr>", opts)
+
+
+
+        -- Fixing indentation problem caused with autopairs
+        local remap = vim.api.nvim_set_keymap
+        local npairs = require('nvim-autopairs')
+        npairs.setup({ map_cr = false })
+
+        -- skip it, if you use another global object
+        _G.MUtils = {}
+
+        -- old version
+        -- MUtils.completion_confirm=function()
+        -- if vim.fn["coc#pum#visible"]() ~= 0 then
+        -- return vim.fn["coc#_select_confirm"]()
+        -- else
+        -- return npairs.autopairs_cr()
+        -- end
+        -- end
+
+        -- new version for custom pum
+        MUtils.completion_confirm = function()
+            if vim.fn["coc#pum#visible"]() ~= 0 then
+                return vim.fn["coc#pum#confirm"]()
+            else
+                return npairs.autopairs_cr()
+            end
+        end
+
+        remap('i', '<CR>', 'v:lua.MUtils.completion_confirm()', { expr = true, noremap = true })
     end
 }
