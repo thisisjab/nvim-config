06111954605524b803a8680ee71b6ea87ac48b06
thisisjab <heythisisjab@gmail.com>
2024-03-26 20:58:02 +0330

    feat: install and configure `aura` theme

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,13 +1,21 @@
 return {
-    { "catppuccin/nvim",            name = "catppuccin",  priority = 1000 },
-    { "morhetz/gruvbox",            name = "gruvbox",     priority = 1000 },
-    { "mofiqul/vscode.nvim",        name = "vscode",      priority = 1000 },
-    { "oxfist/night-owl.nvim",      name = "nightowl",    priority = 1000 },
-    { "folke/tokyonight.nvim",      name = "tokyonight",  priority = 1000 },
-    { "rebelot/kanagawa.nvim",      name = "kangawa",     priority = 1000 },
-    { "bluz71/vim-nightfly-colors", name = "nightfly",    priority = 1000 },
-    { "bluz71/vim-moonfly-colors",  name = "moonfly",     priority = 1000 },
-    { "rose-pine/neovim",           name = "rose-pine",   priority = 1000 },
-    { "EdenEast/nightfox.nvim",     name = "nightfox",    priority = 1000 },
+    { "catppuccin/nvim",            name = "catppuccin", priority = 1000 },
+    { "morhetz/gruvbox",            name = "gruvbox",    priority = 1000 },
+    { "mofiqul/vscode.nvim",        name = "vscode",     priority = 1000 },
+    { "oxfist/night-owl.nvim",      name = "nightowl",   priority = 1000 },
+    { "folke/tokyonight.nvim",      name = "tokyonight", priority = 1000 },
+    { "rebelot/kanagawa.nvim",      name = "kangawa",    priority = 1000 },
+    { "bluz71/vim-nightfly-colors", name = "nightfly",   priority = 1000 },
+    { "bluz71/vim-moonfly-colors",  name = "moonfly",    priority = 1000 },
+    { "rose-pine/neovim",           name = "rose-pine",  priority = 1000 },
+    { "EdenEast/nightfox.nvim",     name = "nightfox",   priority = 1000 },
+    {
+        "baliestri/aura-theme",
+        lazy = false,
+        priority = 1000,
+        config = function(plugin)
+            vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+        end
+    },
     -- { "RRethy/base16-nvim",         name = "base16-themes", priority = 1000 },
 }
